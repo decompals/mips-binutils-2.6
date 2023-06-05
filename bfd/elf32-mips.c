674cc7b729c6ef014f7e9ea8a2fbb6897a4b2f5b
pixel-stuck <mathmcclintic@gmail.com>
2023-06-05 17:14:36 -0400

    Fix shtab bug and add option to re-enable it (#10)

--- bfd/elf32-mips.c
+++ bfd/elf32-mips.c
@@ -985,13 +985,21 @@ bfd_mips_elf32_swap_gptab_out (abfd, in, ex)
    symbols.  On most ELF targets the split is between static symbols
    and externally visible symbols.  */
 
+boolean gIrixSymtab = false;
+
 /*ARGSUSED*/
 static boolean
 mips_elf_sym_is_global (abfd, sym)
      bfd *abfd;
      asymbol *sym;
 {
-  return (sym->flags & BSF_SECTION_SYM) == 0 ? true : false;
+  if (gIrixSymtab == true) {
+    return (sym->flags & BSF_SECTION_SYM) == 0 ? true : false;
+  } else {
+    return ((sym->flags & (BSF_GLOBAL | BSF_WEAK)) != 0
+        || bfd_is_und_section (sym->section)
+        || bfd_is_com_section (sym->section));
+  }
 }
 
 /* Set the right machine number for a MIPS ELF file.  */
